94a1bf93dd7b49b49188ba5b54ffccf52ee59ace
Juan Iscar <juan.iscar@exponentiateam.com>
2021-02-27 13:39:53 +0100

    Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- test/dbs/postgres/postgres.go
+++ test/dbs/postgres/postgres.go
@@ -1,12 +1,7 @@
 package postgres
 
 import (
-	//"github.com/jinzhu/gorm"
-
 	databases "github.com/juaismar/go-gormssp/test/dbs"
-	//"github.com/juaismar/go-gormssp/test/dbs/postgres"
-
-	//"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
